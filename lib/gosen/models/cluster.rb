7fe7b1bca04d61a6a7314a82e818a5bcbbdc95e9
Pierre Riteau <priteau@gmail.com>
2009-07-16 18:59:40 +0200

    Add support for cluster status.

--- lib/gosen/models/cluster.rb
+++ lib/gosen/models/cluster.rb
@@ -21,5 +21,42 @@ module Gosen
       end
       return @nodes.sort! { |a,b| a.uid <=> b.uid }
     end
+
+    class Status
+      include Model
+      attr_accessor :hardware, :system, :uri, :generated_at
+
+      def initialize(hash)
+        populate_from_hash!(hash)
+        @hardware = Gosen::Site::Status::Hardware.new(hash['aggregated_nodes_stats']['hardware_state'])
+        @system = Gosen::Site::Status::System.new(hash['aggregated_nodes_stats']['system_state'])
+      end
+
+      class System
+        include Model
+        attr_accessor :busy, :besteffort, :free, :unknown
+
+        def initialize(hash)
+          @busy = @besteffort = @free = @unknown = 0
+          populate_from_hash!(hash)
+        end
+      end
+
+      class Hardware
+        include Model
+        attr_accessor :alive, :dead, :absent, :suspected
+
+        def initialize(hash)
+          @alive = @dead = @absent = @suspected = 0
+          populate_from_hash!(hash)
+        end
+      end
+    end
+
+    def status
+      base_uri = @uri.sub(/\/versions\/[a-f0-9]+/, '')
+      h = JSON.parse(Gosen::Session.monitoring["#{base_uri}/statuses/current"].get(:accept => 'application/json'))
+      return Gosen::Cluster::Status.new(h)
+    end
   end
 end
