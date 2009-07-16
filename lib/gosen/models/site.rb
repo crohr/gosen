b3531b5373decbc85c9732e364baeb8c13c6a120
Pierre Riteau <priteau@gmail.com>
2009-07-16 07:52:48 +0200

    Add support for site status.
    Doesn't work reliably yet because zero'd fields are not sent in the JSON.

--- lib/gosen/models/site.rb
+++ lib/gosen/models/site.rb
@@ -21,5 +21,31 @@ module Gosen
       end
       return @clusters.sort! { |a,b| a.uid <=> b.uid }
     end
+
+    class Status
+      include Model
+      attr_accessor :hardware, :system
+
+      def initialize(hash)
+        populate_from_hash!(hash)
+        @hardware = Gosen::Site::Status::Hardware.new(hash['hardware_state'])
+        @system = Gosen::Site::Status::System.new(hash['system_state'])
+      end
+
+      class System
+        include Model
+        attr_accessor :busy, :besteffort, :free, :unknown
+      end
+
+      class Hardware
+        include Model
+        attr_accessor :alive, :dead, :absent
+      end
+    end
+
+    def status
+      h = JSON.parse(Gosen::Session.session["/sites/#{@name.downcase}/statuses/current"].get(:accept => 'application/json'))
+      return Gosen::Site::Status.new(h['aggregated_nodes_stats'])
+    end
   end
 end
